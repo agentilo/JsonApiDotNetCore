a1c7287275519c878929a8f4251f3fb7af752ff0
maurei <maurits.moeys@gmail.com>
2021-12-06 15:39:18 +0100

    Fix failing test when run in parallel, respect naming convention as set in SerializerSettings for properties in swagger doc

---
+++ test/OpenApiTests/NamingConvention/SupermarketsController.cs
@@ -0,0 +1,16 @@
+using JsonApiDotNetCore.Configuration;
+using JsonApiDotNetCore.Controllers;
+using JsonApiDotNetCore.Services;
+using Microsoft.Extensions.Logging;
+
+namespace OpenApiTests.NamingConvention
+{
+    public sealed class SupermarketsController : JsonApiController<Supermarket, int>
+    {
+        public SupermarketsController(IJsonApiOptions options, IResourceGraph resourceGraph, ILoggerFactory loggerFactory,
+            IResourceService<Supermarket, int> resourceService)
+            : base(options, resourceGraph, loggerFactory, resourceService)
+        {
+        }
+    }
+}
